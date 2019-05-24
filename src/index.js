45c58134a9696f7e99a951ab0d838d08acea3ab8
lekesiz <ahmetlekesiz@gmail.com>
2019-05-25 02:41:53 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={i*2} />;
   }
 
   render() {
